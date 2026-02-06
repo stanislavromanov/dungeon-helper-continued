36eaa62ea3d478b28ac5e369cef591c48e35a825
stanislavromanov <stanislav.romanov@outlook.com>
2026-02-06 18:49:22 +0100

    Initial commit: DungeonHelperContinued WoW addon

---
+++ DungeonHelperContinued.lua
@@ -0,0 +1,88 @@
+local addonName, ns = ...
+
+local defaults = {
+    greetingMessage = "hi",
+    goodbyeMessage = "thanks, bb",
+    reportTimeToParty = true,
+}
+
+local startTime = nil
+
+local function FormatTime(seconds)
+    local h = math.floor(seconds / 3600)
+    local m = math.floor((seconds % 3600) / 60)
+    local s = math.floor(seconds % 60)
+
+    if h > 0 then
+        return string.format("%dh %dm %ds", h, m, s)
+    elseif m > 0 then
+        return string.format("%dm %ds", m, s)
+    else
+        return string.format("%ds", s)
+    end
+end
+
+StaticPopupDialogs["DUNGEONHELPER_LEAVE_CONFIRM"] = {
+    text = "Do you want to leave the instance?",
+    button1 = "Yes",
+    button2 = "No",
+    OnAccept = function()
+        C_ChatInfo.SendChatMessage(DungeonHelperContinuedDB.goodbyeMessage, "INSTANCE_CHAT")
+        C_PartyInfo.LeaveParty(LE_PARTY_CATEGORY_INSTANCE)
+    end,
+    timeout = 0,
+    whileDead = true,
+    hideOnEscape = true,
+    preferredIndex = 3,
+}
+
+local frame = CreateFrame("Frame")
+frame:RegisterEvent("ADDON_LOADED")
+frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+frame:RegisterEvent("LFG_COMPLETION_REWARD")
+
+frame:SetScript("OnEvent", function(self, event, ...)
+    if event == "ADDON_LOADED" then
+        local loadedAddon = ...
+        if loadedAddon ~= addonName then return end
+
+        if DungeonHelperContinuedDB == nil then
+            DungeonHelperContinuedDB = {}
+        end
+        for k, v in pairs(defaults) do
+            if DungeonHelperContinuedDB[k] == nil then
+                DungeonHelperContinuedDB[k] = v
+            end
+        end
+
+        self:UnregisterEvent("ADDON_LOADED")
+
+    elseif event == "PLAYER_ENTERING_WORLD" then
+        local isInitialLogin, isReloadingUi = ...
+        if isInitialLogin or isReloadingUi then return end
+
+        if not IsInGroup(LE_PARTY_CATEGORY_INSTANCE) then return end
+
+        local _, instanceType = GetInstanceInfo()
+        if instanceType ~= "party" and instanceType ~= "raid" then return end
+
+        startTime = GetTime()
+        C_ChatInfo.SendChatMessage(DungeonHelperContinuedDB.greetingMessage, "INSTANCE_CHAT")
+
+    elseif event == "LFG_COMPLETION_REWARD" then
+        local elapsed = 0
+        if startTime then
+            elapsed = GetTime() - startTime
+        end
+
+        local timeStr = FormatTime(elapsed)
+
+        if DungeonHelperContinuedDB.reportTimeToParty then
+            C_ChatInfo.SendChatMessage("Dungeon completed in: " .. timeStr, "INSTANCE_CHAT")
+        else
+            print("Dungeon completed in: " .. timeStr)
+        end
+
+        StaticPopup_Show("DUNGEONHELPER_LEAVE_CONFIRM")
+    end
+end)
---
+++ Options.lua
@@ -0,0 +1,82 @@
+local addonName, ns = ...
+
+local function CreateOptionsFrame()
+    local panel = CreateFrame("Frame")
+    panel.name = "Dungeon Helper Continued"
+    panel:Hide()
+
+    local title = panel:CreateFontString(nil, "ARTWORK", "GameFontNormalLarge")
+    title:SetPoint("TOPLEFT", 16, -16)
+    title:SetText("Dungeon Helper Continued")
+
+    -- Greeting Message
+    local greetingLabel = panel:CreateFontString(nil, "ARTWORK", "GameFontNormal")
+    greetingLabel:SetPoint("TOPLEFT", title, "BOTTOMLEFT", 0, -24)
+    greetingLabel:SetText("Greeting Message:")
+
+    local greetingBox = CreateFrame("EditBox", "DHCGreetingEditBox", panel, "InputBoxTemplate")
+    greetingBox:SetSize(200, 20)
+    greetingBox:SetPoint("TOPLEFT", greetingLabel, "BOTTOMLEFT", 6, -4)
+    greetingBox:SetAutoFocus(false)
+    greetingBox:SetScript("OnEnterPressed", function(self)
+        DungeonHelperContinuedDB.greetingMessage = self:GetText()
+        self:ClearFocus()
+    end)
+    greetingBox:SetScript("OnEscapePressed", function(self)
+        self:SetText(DungeonHelperContinuedDB.greetingMessage)
+        self:ClearFocus()
+    end)
+
+    -- Goodbye Message
+    local goodbyeLabel = panel:CreateFontString(nil, "ARTWORK", "GameFontNormal")
+    goodbyeLabel:SetPoint("TOPLEFT", greetingBox, "BOTTOMLEFT", -6, -16)
+    goodbyeLabel:SetText("Goodbye Message:")
+
+    local goodbyeBox = CreateFrame("EditBox", "DHCGoodbyeEditBox", panel, "InputBoxTemplate")
+    goodbyeBox:SetSize(200, 20)
+    goodbyeBox:SetPoint("TOPLEFT", goodbyeLabel, "BOTTOMLEFT", 6, -4)
+    goodbyeBox:SetAutoFocus(false)
+    goodbyeBox:SetScript("OnEnterPressed", function(self)
+        DungeonHelperContinuedDB.goodbyeMessage = self:GetText()
+        self:ClearFocus()
+    end)
+    goodbyeBox:SetScript("OnEscapePressed", function(self)
+        self:SetText(DungeonHelperContinuedDB.goodbyeMessage)
+        self:ClearFocus()
+    end)
+
+    -- Report Time Checkbox
+    local reportCheck = CreateFrame("CheckButton", "DHCReportCheckButton", panel, "InterfaceOptionsCheckButtonTemplate")
+    reportCheck:SetPoint("TOPLEFT", goodbyeBox, "BOTTOMLEFT", -8, -16)
+    reportCheck.Text:SetText("Report time to party")
+    reportCheck:SetScript("OnClick", function(self)
+        DungeonHelperContinuedDB.reportTimeToParty = self:GetChecked()
+    end)
+
+    -- Refresh values when panel is shown
+    panel:SetScript("OnShow", function()
+        greetingBox:SetText(DungeonHelperContinuedDB.greetingMessage or "Hi")
+        goodbyeBox:SetText(DungeonHelperContinuedDB.goodbyeMessage or "thanks, bb")
+        reportCheck:SetChecked(DungeonHelperContinuedDB.reportTimeToParty)
+    end)
+
+    return panel
+end
+
+local frame = CreateFrame("Frame")
+frame:RegisterEvent("ADDON_LOADED")
+frame:SetScript("OnEvent", function(self, event, loadedAddon)
+    if loadedAddon ~= addonName then return end
+    self:UnregisterEvent("ADDON_LOADED")
+
+    local panel = CreateOptionsFrame()
+
+    local category = Settings.RegisterCanvasLayoutCategory(panel, panel.name)
+    category.ID = addonName
+    Settings.RegisterAddOnCategory(category)
+
+    SLASH_DUNGEONHELPERCONTINUED1 = "/dhc"
+    SlashCmdList["DUNGEONHELPERCONTINUED"] = function()
+        Settings.OpenToCategory(category.ID)
+    end
+end)
